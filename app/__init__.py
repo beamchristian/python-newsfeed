1b27334686423f811efbfa9f72ded3b43aa08d63
beamchristianE <ckbeamcoding@gmail.comm>
2022-04-23 12:07:19 -0400

    rendering home page, dashboard, and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from app.routes import home
+from app.routes import home, dashboard
 from flask import Flask
 
 def create_app(test_config=None):
@@ -14,4 +14,5 @@ def create_app(test_config=None):
     return 'hello world'
   # register routes
   app.register_blueprint(home)
+  app.register_blueprint(dashboard)
   return app
